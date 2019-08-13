bb27df14b8095c4172c36c6d643a5c96993e6743
Michael Karrer <michaelkarrer81@gmail.com>
2019-08-13 13:38:20 +0200

    [ADD] __openerp__.py for the default instance addon

---
+++ addons/tral_config/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'tral_config',
+    'summary': '''FS-Online tral instance configuration''',
+    'description': '''
+FS-Online Instance Configuration
+================================
+
+Customer configuration for the instance tral
+
+- Default settings
+- View modifications
+- CSS
+- Translations
+    ''',
+    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'version': '1.0',
+    'website': 'https://www.datadialog.net',
+    'installable': True,
+    'depends': [
+        'fsonline',
+    ],
+    'data': [],
+}
+
+    
